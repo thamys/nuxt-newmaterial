8b3b321d9a86b1944e9a34f98e5391118c604f1e
Thamyres Guedes <thataguedis@gmail.com>
2018-05-27 00:23:46 -0300

    🚧 Color Palete

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,52 +1,57 @@
-<template>
-  <div>
-    <nuxt/>
-  </div>
+<template lang="pug">
+  main
+    nuxt
 </template>
 
-<style>
-html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
 
-*, *:before, *:after {
-  box-sizing: border-box;
-  margin: 0;
-}
+<style lang="sass">
+  html
+    // Colors
+    
+    --color-primary-050: #FFF0E9
+    --color-primary-100: #FFDBCF
+    --color-primary-200: #FCB8AB
+    --color-primary-300: #F2A9A5
+    --color-primary-400: #E29896
+    --color-primary-500: #C18083
+    --color-primary-600: #A36A72
+    --color-primary-700: #84565E
+    --color-primary-800: #664046
+    --color-primary-900: #442B2D
+    --color-primary-main: var(--color-primary-100)
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
+    --color-secondary-050: #FFDBCF
+    --color-secondary-100: #FFCEB9
+    --color-secondary-200: #FEAE8B
+    --color-secondary-300: #FC8F5C
+    --color-secondary-400: #FA7736
+    --color-secondary-500: #F86203
+    --color-secondary-600: #EE5C00
+    --color-secondary-700: #E05400
+    --color-secondary-800: #D24D00
+    --color-secondary-900: #BA4000
+    --color-secondary-main: var(--color-secondary-050)
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
+    --color-surface-base: #FFFBFA
+    --color-surface-dark: #F3EFEE
+    --color-surface-light: #FFFFFF
+    --color-surface-main: var(--color-surface-base)
+    --color-surface-secondary: var(--color-primary-100)
 
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
+    --color-icons-black-active: #000000
+    --color-icons-black-disabled: rgba(0, 0, 0, .38)
+    --color-icons-black-inactive: rgba(0, 0, 0, .54)
+    --color-icons-primary-or-secondary: var(--color-primary-100)
+    --color-icons-selected-onPrimary-active: #442B2D
+    --color-icons-selected-onPrimary-disabled: rgba(0, 0, 0, .38)
+    --color-icons-selected-onPrimary-inactive: rgba(68, 43, 45, .54)
+    --color-icons-selected-onSecondary-active: #442B2D
+    --color-icons-selected-onSecondary-disabled: rgba(0, 0, 0, .38)
+    --color-icons-selected-onSecondary-inactive: rgba(68, 43, 45, .54)
+    --color-icons-selected-onSurface-active: #442B2D
+    --color-icons-selected-onSurface-disabled: rgba(0, 0, 0, .38)
+    --color-icons-selected-onSurface-inactive: rgba(68, 43, 45, .54)
+    --color-icons-white-active: #FFFFFF
+    --color-icons-white-disabled: rgba(255, 255, 255, .38)
+    --color-icons-white-inactive: rgba(255, 255, 255, .54)
 </style>
